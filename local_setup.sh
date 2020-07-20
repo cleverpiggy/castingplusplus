8ace0e7bd12a48fffb2ce895a259e2979d2f14cc
Allen Boyd Cunningham <cleverpiggy1@mac.com>
2020-07-20 15:13:57 -0700

    feat:  Add a cute picture to home page.

--- local_setup.sh
+++
@@ -1,4 +0,0 @@
-#edit DATABASE_URL to reflect the local database you created
-export DATABASE_URL='postgresql://cleverpiggy@localhost:5432/castingdb'
-export FLASK_APP=flaskr
-export FLASK_ENV=development
