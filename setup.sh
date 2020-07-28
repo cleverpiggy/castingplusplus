60b0566c061d8e7a73eff8df720dee570fecb981
Allen Boyd Cunningham <cleverpiggy1@mac.com>
2020-07-27 17:02:56 -0700

    chore: Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,13 @@
+export DATABASE_URL='postgresql://localhost:5432/castingdb'
+
+export FLASK_APP=flaskr
+
+export AUTH0_CLIENT_ID='pYc6zi9lp77mOaG3qo1eFArPD89myF2S'
+export AUTH0_DOMAIN='cleverpiggy.auth0.com'
+export AUTH0_CALLBACK_URL='http://127.0.0.1:5000/callback'
+export AUTH0_AUDIENCE='https://castingplusplus.herokuapp.com/'
+export AUTH0_CLIENT_SECRET='Hk94W5S3EK0KmqId-mIjVL5ypcMt3NyeexX2IdtBKPMTSgieiyqc6EMTp5Z9HjDI'
+
+export ASSISTANT_INFO='assistant@assistant.com,3l4djkt#L'
+export DIRECTOR_INFO='director@director.com,;lk43qJK'
+export PRODUCER_INFO='producer@producer.com,l;sadkjf#$JT:'
